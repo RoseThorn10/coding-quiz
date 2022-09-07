1d3614fdf5116403412ac526904ce47889ec3474
RoseThorn10 <lrroman@comcast.net>
2022-09-06 22:21:37 -0500

    Edited index.html, questions.js, and script.js

---
+++ assets/javascript/questions.js
@@ -0,0 +1,12 @@
+var questions = [
+    {
+    title: "What does HTML stand for?",
+    choices: ["He Takes Monkey Lumps", "Hamburger Tomato Mustard Lettuce"],
+    answer: "Hypertext Markup Language"
+    },
+    {
+        title: "What does JavaScript do?",
+        choices: ["Makes my morning coffee", "Adds styling to a web page"],
+        answer: "Adds interaction to a web page"
+    }
+]
---
+++ assets/javascript/script.js
@@ -0,0 +1,72 @@
+
+const numQuestions = questions.length;
+var numAnswers = 3;
+
+var score = 0;
+var counter = 30;
+var questionNum = 0;
+
+
+var ansclass = document.getElementsByClassName("ans");
+
+var ansclicked = function(e) {
+    if (choose(e)) {
+       // alert('Correct!');
+        score+=1;
+    } else {
+        counter -= 5;
+        //alert('Incorrect');
+    }
+
+    questionNum+=1;
+    if (questionNum < numQuestions) {
+        setQuestion(questionNum);
+    } else {
+        // Do game over man!
+    }
+}
+
+for (i = 0; i < ansclass.length; i++) {
+    ansclass[i].addEventListener("click", ansclicked, false);
+}
+
+var startBtn = document.getElementById("start");
+
+startBtn.addEventListener("click", startQuiz);
+
+function startQuiz() {
+    document.getElementById("start-screen").style.display = "none";
+    
+    var timer = setInterval    
+    (function() {
+        counter--;
+        document.getElementById("counter").textContent = counter;
+        if (counter <= 0) {
+            clearInterval(timer);
+            // do game over
+        }
+    }, 1000);
+
+    setQuestion(questionNum);
+}
+
+function setQuestion(questionNumber) {
+    document.getElementById("question").textContent = questions[questionNumber].title;
+
+    var ans = Math.ceil(Math.random() * numAnswers);
+    document.getElementById("ans" + ans).textContent = questions[questionNumber].answer;
+    count = 0;
+    for (i = 1; i <= numAnswers; i++) {
+        if (i == ans)
+            continue;
+
+        document.getElementById("ans" + i).textContent = questions[questionNumber].choices[count];
+        count++;
+    }
+}
+
+function choose(e) {
+    var answer = questions[questionNum].answer;
+    return (answer == e.target.textContent);
+    
+}
